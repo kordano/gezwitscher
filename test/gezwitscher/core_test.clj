2c7837a4edd2fe3d5fd935b5d3a79a0c38b77689
kordano <konrad.kuehne@rocketmail.com>
2014-05-27 13:16:17 +0200

    Add some explanations

--- test/gezwitscher/core_test.clj
+++ test/gezwitscher/core_test.clj
@@ -5,17 +5,18 @@
 
 ;; example state, never reveal credentials in code, store the in env vars
 (def twitter-state
-  (atom
-   {:credentials {:consumer-key (or (System/getenv "TWITTER_API_KEY") "")
-                  :consumer-secret (or (System/getenv "TWITTER_API_SECRET") "")
-                  :access-token (or (System/getenv "TWITTER_ACCESS_TOKEN") "")
-                  :access-token-secret (or (System/getenv "TWITTER_ACCESS_TOKEN_SECRET") "")}
-    :handler (fn [status] (println (-> status :user :screen_name) " posts "(:text status)))
-    :follow [1460703391]
-    :track ["clojure" "functional" "programming"]}))
+  {:credentials {:consumer-key (or (System/getenv "TWITTER_API_KEY") "****")
+                 :consumer-secret (or (System/getenv "TWITTER_API_SECRET") "****")
+                 :access-token (or (System/getenv "TWITTER_ACCESS_TOKEN") "****")
+                 :access-token-secret (or (System/getenv "TWITTER_ACCESS_TOKEN_SECRET") "****")}
+   :handler (fn [status] (println (:text status)))
+   :follow [1460703391]
+   :track ["clojure" "functional" "programming"]})
 
 
 (defn test-stream []
   (let [stop-stream (start-filter-stream @twitter-state)]
     (Thread/sleep 20000)
     (stop-stream)))
+
+(def stop-stream (start-filter-stream twitter-state))
