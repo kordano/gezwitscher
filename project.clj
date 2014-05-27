153e007bdac5212ed320fda7e93d21083a432c67
kordano <konrad.kuehne@rocketmail.com>
2014-05-27 13:29:07 +0200

    add scm to project

--- project.clj
+++ project.clj
@@ -10,4 +10,7 @@
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [org.clojure/data.json "0.2.4"]
                  [org.twitter4j/twitter4j-core "4.0.1"]
-                 [org.twitter4j/twitter4j-stream "4.0.1"]])
+                 [org.twitter4j/twitter4j-stream "4.0.1"]]
+  :scm {:name "git"
+        :tag "2c7837a4edd2fe3d5fd935b5d3a79a0c38b77689"
+        :url "https://github.com/kordano/gezwitscher.git"})
