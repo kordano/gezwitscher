f05d1114d1dc736589f339f5e03285fe6df587b0
kordano <konrad.kuehne@rocketmail.com>
2014-06-05 23:27:37 +0200

    change parameter list of fetch-stream

--- src/gezwitscher/core.clj
+++ src/gezwitscher/core.clj
@@ -6,7 +6,6 @@
 
 (set! *warn-on-reflection* true)
 
-
 (defn- build-config
   "Twitter stream configuration"
   ^Configuration [{:keys [consumer-key consumer-secret access-token access-token-secret]}]
@@ -48,11 +47,11 @@
 
 
 (defn start-filter-stream
-  "Starts streaming, following given ids and tracking given keywords"
-  [stream-state]
-  (let [filter-query (FilterQuery. 0 (long-array (:follow stream-state)) (into-array String (:track stream-state)))
-        stream (get-twitter-stream-factory (:credentials stream-state))]
-    (.addListener stream (status-listener (:handler stream-state)))
+  "Starts streaming, following given ids, tracking given keywords, handling incoming tweets with provided handler function"
+  [follow track handler credentials]
+  (let [filter-query (FilterQuery. 0 (long-array follow) (into-array String track))
+        stream (get-twitter-stream-factory credentials)]
+    (.addListener stream (status-listener handler))
     (.filter stream filter-query)
     (fn [] (do
             (.shutdown stream)
