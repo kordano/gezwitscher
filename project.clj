beb7db4ef624da40916800f24a3d301fa75774ef
kordano <konrad.kuehne@rocketmail.com>
2014-08-15 11:12:17 +0200

    upgrade dependencies

--- project.clj
+++ project.clj
@@ -8,9 +8,10 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
 
   :dependencies [[org.clojure/clojure "1.5.1"]
-                 [org.clojure/data.json "0.2.4"]
-                 [org.twitter4j/twitter4j-core "4.0.1"]
-                 [org.twitter4j/twitter4j-stream "4.0.1"]]
+                 [org.clojure/core.async "0.1.303.0-886421-alpha"]
+                 [org.clojure/data.json "0.2.5"]
+                 [org.twitter4j/twitter4j-core "4.0.2"]
+                 [org.twitter4j/twitter4j-stream "4.0.2"]]
 
   :scm {:name "git"
         :tag "6e0797a142049fe07d395db7b0dbdd4da3fa34fc"
