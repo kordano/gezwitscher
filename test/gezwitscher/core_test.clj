1a3496d07c02e19eac38d7f5ef08806f1a64e80b
kordano <konrad.kuehne@rocketmail.com>
2014-08-15 12:18:16 +0200

    adjust stream test

--- test/gezwitscher/core_test.clj
+++ test/gezwitscher/core_test.clj
@@ -29,7 +29,22 @@
 
 (comment
 
-  (def stop-stream (start-filter-stream @test-state))
+  (def creds {:consumer-key (or (System/getenv "TWITTER_API_KEY") "****")
+              :consumer-secret (or (System/getenv "TWITTER_API_SECRET") "****")
+              :access-token (or (System/getenv "TWITTER_ACCESS_TOKEN") "****")
+              :access-token-secret (or (System/getenv "TWITTER_ACCESS_TOKEN_SECRET") "****")})
+
+  (def track ["@FAZ_NET" "@tagesschau" "@dpa" "@SZ" "@SPIEGELONLINE" "@BILD" "@DerWesten" "@ntvde" "@tazgezwitscher" "@welt" "@ZDFheute" "@N24_de" "@sternde" "@focusonline"])
+
+  (def follow [114508061 18016521 5734902 40227292 2834511 9204502 15071293 19232587 15243812 8720562 1101354170 15738602 18774524 5494392])
+
+
+  (let [stream (start-filter-stream test-credentials :track track :follow follow)]
+    (def stop-strean (:stop-fn stream))
+    (go-loop [ch (:status-chan stream)]
+      (let [status (<! ch)]
+        (println (:text status))
+        (recur ch))))
 
   (stop-stream)
 
